05e20b518d8f11f01639fe22ef624133382cf300
mauricio <jourdanmauricio@gmail.com>
2023-09-02 03:29:46 -0300

    .

--- src/components/Notes/ViewNotes.jsx
+++ src/components/Notes/ViewNotes.jsx
@@ -3,14 +3,14 @@ import ViewNote from './ViewNote';
 const ViewNotes = ({ notes, onChangeAction, onDelete, onUpdate, onDetail }) => {
   return (
     <>
-      <section className="min-h-[320px] text-gray-900 dark:text-slate-100 bg-slate-100 dark:bg-gray-900 p-2">
+      <section className="flex flex-col min-h-[320px] text-gray-900 dark:text-slate-100 bg-slate-100 dark:bg-gray-900 p-2">
         <div className="relative">
           <lumau-message
             id="form-error"
             errorForm=""
           ></lumau-message>
         </div>
-        <div className="flex flex-row flex-wrap gap-4 justify-center items-center mt-8">
+        <div className="flex flex-grow flex-row flex-wrap gap-4 justify-center items-center mt-8">
           {notes.map((note) => (
             <ViewNote
               key={note.id}
@@ -21,15 +21,13 @@ const ViewNotes = ({ notes, onChangeAction, onDelete, onUpdate, onDetail }) => {
             ></ViewNote>
           ))}
         </div>
-        <div>
-          <button
-            onClick={() => onChangeAction('NEW')}
-            className="block p-2 mt-4 ml-auto bg-violet-800 rounded text-white transition ease-in-out delay-100 hover:bg-purple-800/70
+        <button
+          onClick={() => onChangeAction('NEW')}
+          className="block p-2 mt-4 ml-auto bg-violet-800 rounded text-white transition ease-in-out delay-100 hover:bg-purple-800/70
           hover:cursor-pointer"
-          >
-            Nueva
-          </button>
-        </div>
+        >
+          Nueva
+        </button>
       </section>
     </>
   );
